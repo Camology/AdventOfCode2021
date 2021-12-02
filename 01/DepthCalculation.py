45293e995bc52d512970a9997a5bf47d2da5d9cc
Camology <wirelessatari@gmail.com>
2021-12-02 18:30:02 -0500

    day 1 part 2

--- 01/DepthCalculation.py
+++ 01/DepthCalculation.py
@@ -1,12 +1,28 @@
+# increaseCounter = 0
+# with open('input.txt') as input:
+#     lines = input.readlines()
+# previousLine = lines[0]
+
+# for line in lines:
+#     if (int(line) > int(previousLine)):
+#         increaseCounter+=1
+#     previousLine = line
+
+# print(increaseCounter)
+
 increaseCounter = 0
+position = 0
+intList = []
 with open('input.txt') as input:
     lines = input.readlines()
-previousLine = lines[0]
-
-for line in lines:
-    if (int(line) > int(previousLine)):
-        increaseCounter+=1
-    previousLine = line
-
+    for line in lines:
+        intList.append(int(line))
+previousSum = sum(intList[position:position+3])
+for int in intList:
+    if len(intList[position:position+3]) == 3:
+        a = sum(intList[position:position+3])
+        if (a > previousSum):
+            increaseCounter+=1
+        previousSum = a
+        position+=1
 print(increaseCounter)
-
